3d15aacd6e82fb16b84524236e7682940e6ae464
redyf <mateusalvespereira7@gmail.com>
2026-01-09 18:04:13 -0300

    feat: add ReadFilmeDto

---
+++ Data/Dtos/ReadFilmeDto.cs
@@ -0,0 +1,14 @@
+namespace FilmesApi.Data.Dtos;
+
+public class ReadFilmeDto
+{
+  public string Titulo { get; set; }
+
+  public string Genero { get; set; }
+
+  public int Duracao { get; set; }
+
+  public string Diretor { get; set; }
+
+  public DateTime HoraDaConsulta { get; set; } = DateTime.Now;
+}
